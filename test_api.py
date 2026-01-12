e849f3a871cfc548019d60f67db699a08b527dfa
Prateek <prateekjain199686@gmail.com>
2026-01-12 12:40:20 +0530

    token

--- test_api.py
+++ test_api.py
@@ -20,7 +20,7 @@ payloads={
 
 baseurl="https://gorest.co.in/public/v2/users" #Base URl
 
-token="5a3a0dc0493f9577761fd2395d9f8a380ce179da3dc75781ee44ff6e94fdf638" #Authorization Token
+token="" #Authorization Token
 
 headers={
     "Authorization":f"Bearer {token}",
